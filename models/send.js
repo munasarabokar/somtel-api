b9a01f9b847ab2168b0997bb78481d8d4bd184af
Munasar abuukar <munasarabokar@gmail.com>
2023-02-13 11:30:32 +0300

    updated sender by adding time out

--- models/send.js
+++ models/send.js
@@ -2,16 +2,18 @@ import db  from '../database/data.js'
 
 const sendAll = (index, amounts) =>
   new Promise((resolve, reject) => {
-    const select  =  `SELECT * FROM macaamiil WHERE h_number = ${index}`;
-    db.query(select , (err , respon) => {
+    setTimeout(() => {
+      const select  =  `SELECT * FROM macaamiil WHERE h_number = ${index}`;
+      db.query(select , (err , respon) => {
         if(err) return err
-            if (respon.length > 0) {
-                const data =   `*831*${respon[0].s_number}*${amounts}*4683#`;
-                resolve(data);
-            } else {
+          if (respon.length > 0) {
+              const data =   `*831*${respon[0].s_number}*${amounts}*4683#`;
+              resolve(data);
+          } else {
             resolve("");
-            }
-        })
+          }
+      });
+    }, 4000)
   });
 
 
