6814b8d2f7357b5cfe920d4187414857055b30b8
Munasar abuukar <munasarabokar@gmail.com>
2023-02-15 10:12:47 +0300

    added check types

--- controller/api.js
+++ controller/api.js
@@ -3,7 +3,8 @@ import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-d
 import sendAll from '../models/send.js';
 dotenv.config()
 
-
+// git config --global user.email "abdihalim415@gmail.com" 
+//   git config --global user.name "Munasar Abuukar"
 // get list all
 export const get_tracker = (req , res ) => {
     // check if api is vali
@@ -108,8 +109,7 @@ db.query(find_key , value , (err , find_keys ) => {
 export const send_tracker = (req , res ) => {
     // massage body
     const massage_body = req.body.list;
-    const one = "1 ";
-    // check if api is vali
+    // check if api is valiD
  const find_key = "SELECT * FROM api WHERE  api_key = ? ";
  const value = [req.params.link];
  // db query 
@@ -118,38 +118,103 @@ export const send_tracker = (req , res ) => {
     const check = '';
    if(err) res.send({massage: err });
    if (find_keys.length > 0) { 
-                    // check if 
-        if (0.5 == massage_body.substring(20, 23)) {
-            // daily
-           const numbers = massage_body.substring(35, 44)
-           const amounts = '05'
-           sendAll(numbers, amounts).then(result => {
-            /* process */
-           res.json(result)
-          });
-        } else if ("1 " == massage_body.substring(20, 22)) {
-            // two days
-            const numbers = massage_body.substring(33, 42)
-            const amounts = '1'
-            sendAll(numbers, amounts).then(result => {
-                /* process */
-               res.json(result)
-              });
-        } else if (2.5 == massage_body.substring(20, 23)) {
-            //week 
-            const numbers = massage_body.substring(35, 44)
-            const amounts = '2*5'
-            sendAll(numbers, amounts).then(result => {
-                /* process */
-               res.json(result)
-              });
-        } else {
-            res.json('*301#')
-        }
-
+        // check if 
+        const check_massage = 'SELECT * FROM amounts';
+          db.query(check_massage , (err , check_amounts) => {
+            if(err) res.json(err);
+            
+            if (check_amounts[0].amount == massage_body.substring(
+                check_amounts[0].start_amount , 
+                check_amounts[0].end_amount
+                )) {
+               const numbers = massage_body.substring(
+                check_amounts[0].start_num , 
+                check_amounts[0].end_num
+                )
+                sendAll(numbers, find_keys[0].pin, check_amounts[0].amount).then(result => {
+                    /* process */
+                res.json(result)
+                });
+            } else if (check_amounts[1].amount == massage_body.substring(
+                check_amounts[1].start_amount , 
+                check_amounts[1].end_amount
+                )) {
+                const numbers = massage_body.substring(
+                check_amounts[1].start_num , 
+                check_amounts[1].end_num
+                )
+                sendAll(numbers, find_keys[0].pin, check_amounts[1].amount).then(result => {
+                    /* process */
+                res.json(result)
+                });
+            } else if (check_amounts[2].amount == massage_body.substring(
+                check_amounts[2].start_amount , 
+                check_amounts[2].end_amount
+                )) {
+                const numbers = massage_body.substring(
+                check_amounts[2].start_num , 
+                check_amounts[2].end_num
+                )
+                sendAll(numbers, find_keys[0].pin, check_amounts[2].amount).then(result => {
+                    /* process */
+                res.json(result)
+                });
+            }else if (check_amounts[3].amount == massage_body.substring(
+                check_amounts[3].start_amount , 
+                check_amounts[3].end_amount
+                )) {
+                const numbers = massage_body.substring(
+                check_amounts[3].start_num , 
+                check_amounts[3].end_num
+                )
+                sendAll(numbers, find_keys[0].pin, check_amounts[3].amount).then(result => {
+                    /* process */
+                res.json(result)
+                });
+            } else if (check_amounts[4].amount == massage_body.substring(
+                check_amounts[4].start_amount , 
+                check_amounts[4].end_amount
+                )) {
+                const numbers = massage_body.substring(
+                check_amounts[4].start_num , 
+                check_amounts[4].end_num)
+                sendAll(numbers, find_keys[0].pin, check_amounts[4].amount).then(result => {
+                    /* process */
+                res.json(result)
+                });
+            } else if (check_amounts[5].amount == massage_body.substring(
+                check_amounts[5].start_amount , 
+                check_amounts[5].end_amount
+                )) {
+                const numbers = massage_body.substring(
+                check_amounts[5].start_num , 
+                check_amounts[5].end_num
+                )
+                sendAll(numbers, find_keys[0].pin, check_amounts[5].amount).then(result => {
+                    /* process */
+                res.json(result)
+                });
+            } else {
+                res.json('not oke')
+            }
+          })
+       
    } else {
        res.send({massage : "api key not valid"});
    }
  });
   
+}
+
+export const check = (req , res) => {
+     const check = 'select * from types';
+    db.query(check, (err , ress) => {
+       if (ress.length > 0) {
+        res.json(ress);
+       } else {
+        res.json('not found');
+       }
+    })
+
+    
 }
--- models/send.js
+++ models/send.js
@@ -1,14 +1,38 @@
 import db  from '../database/data.js'
 
-const sendAll = (index, amounts) =>
+const sendAll = (index, pin , amounts) =>
   new Promise((resolve, reject) => {
     setTimeout(() => {
+     
+      const select_amount  =  `SELECT * FROM doorashada WHERE a_r = ${amounts}`;
       const select  =  `SELECT * FROM macaamiil WHERE h_number = ${index}`;
-      db.query(select , (err , respon) => {
-        if(err) return err
-          if (respon.length > 0) {
-              const data =   `*831*${respon[0].s_number}*${amounts}*4683#`;
-              resolve(data);
+      const select_type  =  "SELECT * FROM types WHERE types = ?";
+      db.query(select_amount , (err , resamont) => {
+        if(err)  resolve(err);
+          if (resamont.length > 0) {
+             if (resamont[0].xaalada == 'active') {
+              db.query(select , (err , resp ) => {
+                if(err)  resolve(err);
+                if (resp.length > 0) {
+                 // resolve(resp)
+                  db.query(select_type , [resp[0].types] , (err , send_data ) => {
+                   if(err) resolve(err)
+                   if(send_data.length > 0) {
+                    const data =   `${send_data[0].starts}${resp[0].s_number}*${resamont[0].a_s}*${pin}#`;
+                    resolve(data);
+                   } else {
+                    resolve('*301#')
+                   }
+                  })
+               
+                } else {
+                  resolve('*301#');
+                }
+              })
+             } else {
+              resolve('*301#');
+             }
+             
           } else {
             resolve('*301#');
           }
